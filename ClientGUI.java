71700bb69ba35afa9ff5be2745dcbff94e6e1312
Yuki Saijo <sa1j0y26@gmail.com>
2025-06-05 18:09:26 +0900

    Update UI name

--- ClientGUI.java
+++ ClientGUI.java
@@ -41,7 +41,7 @@ public class ClientGUI extends JFrame {
     private int myPlayerId = -1;
 
     public ClientGUI() {
-        setTitle("Tic-Tac-Toe クライアント");
+        setTitle("スタック式陣取りゲーム");
         setSize(500, 800);
         setDefaultCloseOperation(EXIT_ON_CLOSE);
 
